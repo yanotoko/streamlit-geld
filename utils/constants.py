bfed118491f2c3b8174b19f71ef0618738881cd4
yanotoko <38168268+yanotoko@users.noreply.github.com>
2025-10-08 19:58:29 +0200

    fixed sample headers

--- utils/constants.py
+++ utils/constants.py
@@ -1,13 +1,13 @@
 # utils/constants.py
 
-SAMPLE_CSV = """Category,Bank Account,Amount,Subcategory
+SAMPLE_CSV = """Category,Bank Account,Subcategory,Amount
 Income 1,Utilities,Bank3,100
 Income 1,Travel,Bank1,100
 Income 2,Saving 4,Bank2,100
 Income 2,Saving 5,Bank2,100
 """
 
-DEFAULT_HEADERS = ["Category", "Bank Account", "Amount", "Subcategory"]
+DEFAULT_HEADERS = ["Category", "Bank Account", "Subcategory", "Amount"]
 
 # Upload hardening
 MAX_UPLOAD_MB = 10  # change if you like
